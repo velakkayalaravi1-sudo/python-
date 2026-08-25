58cc7ea84150d2755583cb0b7f2abb573532a87e
velakkayalaravi1-sudo <velakkayalaravi1@gmail.com>
2026-08-25 22:51:29 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+#l=int(input("enter length:"))
+#b=int(input("enter breath:"))
+#area=l*b
+#print("area of recangle is:",area)
+
+#r=float(input("enter radius of the circle:"))
+#area=3.14*r**2
+#print(f"area of the circle is:{area:.3f}")
+
+#print("Good Morning!")
+#name=input("enter your name:")
+#print("nice to meet you",name)
+
+
+
+
+
