46814f6c3c615119fae763fdf597a9a80cb4451f
velakkayalaravi1-sudo <velakkayalaravi1@gmail.com>
2026-08-31 18:20:06 +0530

    first commit

---
+++ conditional.statements.py
@@ -0,0 +1,71 @@
+# age= int(input("enter your age: "))
+
+# if age>=18:
+#     print("eligiable")
+# else:
+#     print("not eligiable")
+
+# n= int(input("enter num: "))
+
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# marks= int(input("enter yours marks: "))
+# if marks<=35:
+#       print("waste of money on studies")
+# elif marks<=50 and marks>=35:
+#       print("better luck next time")
+# elif marks<=80 and  marks>=50:
+#       print("its ok man")
+# elif marks>=80:
+#     print("not bad")
+# else:
+#       print("close the laptop")
+
+# day= int(input("enter day: "))
+# if day==1:
+#     print("monday")
+# elif day==2:
+#     print("tuesday")
+# elif day==3:
+#     print("wednesday")
+# else:
+#     print("holiday")
+
+# a=int(input("enter num; "))
+# b=int(input("enter num; "))
+# sum=a+b
+# if sum>=10 and sum<=19:
+#     print(20)
+# else:
+#     print(sum)
+
+# a=int(input("enter a: "))
+# b=int(input("enter b: "))
+# c=int(input("enter c: "))
+# sum=a+b+c
+# if c>=13:
+#      print(a+b)
+# elif b>=13:
+#      print(a)
+# elif a>=13:
+#      print(0)
+# else:
+#      print(sum)
+
+# a=int(input("enter a: "))
+# b=int(input("enter b: "))
+# c=int(input("enter c: "))
+# sum=a+b+c
+# if a==b==c:
+#     print(0)
+# elif a==b:
+#     print(c)
+# elif a==c:
+#     print(b)
+# elif b==c:
+#     print(a)
+# else:
+#     print(sum)
